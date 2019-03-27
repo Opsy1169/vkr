9e7ecd889e5e8d18b9d46fcf8820ac92ee92442d
Opsy1169 <mynewsuper@gmail.com>
2019-03-27 15:20:18 +0400

    without hecking cmath

--- stuff.py
+++ stuff.py
@@ -26,20 +26,136 @@ pe_param = 5
 output = 0
 
 def Pe(x):
-    return cmath.exp(1j * (x ** 4) + pe_param * 1j * (x ** 2))
+    return np.exp(1j * (x ** 4) + pe_param * 1j * (x ** 2))
 
 
 Aiparam = 10
 
 sigma = 0.1
 def Gauss(x):
-    return cmath.exp(-x**2/sigma**2)
+    return np.exp(-x**2/sigma**2)
 
 def Ai(x):
-    return cmath.exp(I * Aiparam * (x ** 3))
+    return np.exp(1j * Aiparam * (x ** 3))
 
 def AiI(x):
     return exp(I * Aiparam * (x ** 3) + I*x)
+
+def Pe2d(x, y):
+    return np.exp(1j*(x**4 + y**4) + 1j*(x**2 + y**2))
+def Ai2d(x, y):
+    return np.exp(1j*Aiparam*x**3 + 1j*Aiparam*y**3)
+
+def getInitPe2d():
+    xright = 2
+    xleft = -2
+    yright = 2
+    yleft = -2
+    nx = 100
+    ny = 100
+    x = np.linspace(xleft, xright, nx)
+    y = np.linspace(yleft, yright, ny)
+    xstep = abs(x[1] - x[0])
+    ystep = abs(y[1] - y[0])
+    xmid = []
+    ymid = []
+    for i in range(nx-1):
+        xmid.append((x[i+1] + x[i])/2)
+        ymid.append((y[i+1] + y[i])/2)
+    xmid = np.array(xmid)
+    ymid = np.array(ymid)
+    Fphase = []
+    for i in range(len(xmid)):
+        xarr = []
+        for j in range(len(ymid)):
+            xarr = np.append(xarr, Pe2d(xmid[i], ymid[j])*xstep*ystep)
+        Fphase.append(xarr)
+    # x, y = np.meshgrid(u, u)
+
+    Fphase = np.array(Fphase)
+    return Fphase, xmid, ymid
+    # x, y = np.meshgrid(x, y)
+    # a= 1
+    # fig = pylab.figure()
+    # axes = Axes3D(fig)
+    # axes.plot_surface(x, y, Fphase, cmap=plt.cm.jet)
+    # pylab.show()
+
+def getInitAi2d():
+    xright = 2
+    xleft = -2
+    yright = 2
+    yleft = -2
+    nx = 101
+    ny = 101
+    x = np.linspace(xleft, xright, nx)
+    y = np.linspace(yleft, yright, ny)
+    xstep = abs(x[1] - x[0])
+    ystep = abs(y[1] - y[0])
+    xmid = []
+    ymid = []
+    for i in range(nx-1):
+        xmid.append((x[i+1] + x[i])/2)
+        ymid.append((y[i+1] + y[i])/2)
+    xmid = np.array(xmid)
+    ymid = np.array(ymid)
+    Fphase = []
+    for i in range(len(xmid)):
+        xarr = []
+        for j in range(len(ymid)):
+            xarr = np.append(xarr, Ai2d(xmid[i], ymid[j])*xstep*ystep)
+        Fphase.append(xarr)
+    # x, y = np.meshgrid(u, u)
+
+    Fphase = np.array(Fphase)
+    return Fphase, xmid, ymid
+
+def Fourier2d():
+
+    init, x, y = getInitAi2d()
+    start = time.clock()
+    # init, x, y = getInitAi2d()
+    uleft = -2
+    uright = 2
+    vleft = -2
+    vright = 2
+
+    nv = 101
+    nu = 101
+    koef = 2 * np.pi / (0.000633 * 1000)
+    u = np.linspace(uleft, uright, nu)
+    v = np.linspace(vleft, vright, nv)
+    output = []
+    len_x = len(init[0])
+    len_y = len(init)
+    koef_mul_i = -1j * koef
+    for i in range(nu):
+        outputline = []
+        u_i = u[i]
+        for j in range(nv):
+            integrateelem = 0
+            v_j = v[j]
+            for k in range(len_x):#x
+                x_k = x[k]
+                x_u = x_k*u_i
+                for p in range(len_y):#y
+                    integrateelem += init[k][p] * np.exp( koef_mul_i *(x_u + y[p]*v_j))
+            outputline.append( integrateelem)
+        output.append(outputline)
+    # output = np.array(output)
+    for i in range(nu):
+        for j in range(nv):
+            a = output[i][j]
+            output[i][j] = abs(a)
+
+    end = time.clock()
+    print("TIME: ", end - start)
+    output = np.array(output)
+    u, v = np.meshgrid(u, v)
+    fig = pylab.figure()
+    axes = Axes3D(fig)
+    axes.plot_surface(u, v, output, cmap=plt.cm.jet)
+    pylab.show()
 
 
 def plotSmthn():
@@ -68,16 +184,18 @@ def plotSmthn():
 def Pearcey():
     a = 1
     b = 2
-    Nx = 801
-    Nu = 401
-    u_left = -4
-    u_right = 4
-    x_left = -4
-    x_right = 4
-    x = np.linspace(x_left, x_right, Nx)
+    Nx = 5001
+    Nu = 3001
+    u_left = -2
+    u_right = 9
+    x_left = -3
+    x_right = 6
+    # zeros = np.zeros(2000)
+    x = np.linspace(0, x_right, Nx)
+    # x = np.concatenate((zeros, x))
     F = [0] * Nu
     u = np.linspace(u_left, u_right, Nu)
-    k = 2 * np.pi / (0.000633 * 1300)
+    k = 2 * np.pi / (0.000633 * 1000)
     start = time.clock()
     Fmid = [0] * (Nx - 1)
     for i in range(Nx - 1):
@@ -85,7 +203,7 @@ def Pearcey():
     for j in range(Nu):
         F[j] = 0
         for i in range(Nx - 1):
-            F[j] += Fmid[i] * cmath.exp((-I*k * (x[i] + x[i + 1]) * u[j]) / 2)
+            F[j] += Fmid[i] * np.exp((-1j * k * (x[i] + x[i + 1]) * u[j]) / 2)
         # F[j] = F[j] * (x_right-x_left) / (Nx)
     print("done")
     F = list(map(lambda x: x*(x_right-x_left) / (Nx), F))
@@ -125,15 +243,50 @@ def Pearcey():
     # plt.show()
 
 
+def halfPearcey():
+    Nx = 7001
+    Nu = 4001
+    u_left = -4
+    u_right = 5
+    x_left = 0
+    x_right = 10
+    x = np.linspace(x_left, x_right, Nx)
+    F = [0] * Nu
+    u = np.linspace(u_left, u_right, Nu)
+    k = 2 * np.pi / (0.000633 * 1000)
+    z = 0
+    xo = 1
+    zc = 1
+    l = 0.000532
+    # k = 2*np.pi/( l )
+    #
+    x0 = l
+    # zc = (2 * k * x0 ** 2)
+    # z = 75*l/8
+
+    for j in range(Nu):
+        F[j] = 0
+        for i in range(Nx - 1):
+            F[j] +=  np.exp((1j * ((x[i] + x[i + 1])/2)**2 * u[j]/x0) + 1j*(1 - z/zc)*((x[i] + x[i + 1])/2)**4)*((x[i+1]) - x[i])
+            # F[j] = F[j] * (x_right-x_left) / (Nx)
+    print("done")
+    F = list(map(lambda x: x * (x_right - x_left) / (Nx), F))
+    Fabs = list(map(abs, F))
+    plt.plot(u, Fabs)
+    plt.grid()
+    # plt.savefig("plots/pe_u(" + str(u_left) + ", " + str(u_right) + ", " + str(Nu) + ")_x(" + str(x_left) + ", " + str(
+    #     x_right) + ", " + str(Nx) + ")_param(" + str(pe_param) + ").png")
+    plt.show()
+
 def Airy():
     a = 3
     b = 1
-    Nx = 401
-    Nu = 401
+    Nx = 2001
+    Nu = 2001
     u_left = -0.1
-    u_right = 7
-    x_left = -2
-    x_right = 2
+    u_right = 15
+    x_left = -4
+    x_right = 4
 
     x = np.linspace(x_left, x_right, Nx)
     F = [0] * Nu
@@ -143,25 +296,26 @@ def Airy():
     # print(x)
 
     # print(u)
+    start = time.clock()
     for j in range(Nu):
         F[j] = 0
         for i in range(Nx - 1):
-            F[j] += Ai((x[i] + x[i + 1]) / 2) * cmath.exp((-I * k * ((x[i] + x[i + 1])) * u[j])/2) * (x[i + 1] - x[i])#сейчас в формуле в экспоненте стоит -I, чтобы было похоже на результат Фурье
-            # print(F[j])
-        # F[j] = F[j] * (2 * a)/(Nx)
-        # print(F[j])
+            F[j] += Ai((x[i] + x[i + 1]) / 2) * np.exp((-1j * k * ((x[i] + x[i + 1])) * u[j])/2) * (x[i + 1] - x[i])#сейчас в формуле в экспоненте стоит -I, чтобы было похоже на результат Фурье
 
     G = [0]*Nu
     # mp.dps = 50
     # for i in range(Nu):
     #     G[i] = Float(abs(mp.quad(lambda x: Ai(x) * cmath.exp(-I * k * x * u[i]), [-1, 3])), 35)
     #     print(G[i])
-    print(F)
     # print(F)
-    print("airy")
-    print(k, F[0], u[0])
+    # print(F)
+    # print("airy")
+    # print(k, F[0], u[0])
+    end = time.clock()
+
+    print("TIME: ", str(end-start))
     Fabs = list(map(abs, F))
-    print(Fabs)
+    # print(Fabs)
 
     F2dAbs = []
 
@@ -170,21 +324,21 @@ def Airy():
         F2dAbs.append(row)
 
     # print(F2dAbs[50][50])
-    arrayF2dAbs = np.array(F2dAbs)
-    print(arrayF2dAbs)
-    x, y = np.meshgrid(u, u)
-
-    fig = pylab.figure()
-    axes = Axes3D(fig)
-    axes.plot_surface(x, y, arrayF2dAbs, cmap=plt.cm.jet)
-    pylab.savefig("plots/ai2d_u(" + str(u_left) + ", " + str(u_right) + ", " + str(Nu) + ")_x(" + str(x_left) + ", " + str(x_right) + ", " + str(Nx) + ")_param(" + str(Aiparam) + ").png")
-    pylab.show()
-    # print("another done")
-    # plt.plot(u, Fabs, u, G)
-    # plt.grid()
-    # plt.legend(['Numeric', 'mp.quad'])
-    # plt.savefig("plots/ai_u(" + str(u_left) + ", " + str(u_right) + ", " + str(Nu) + ")_x(" + str(x_left) + ", " + str(x_right) + ", " + str(Nx) + ").png")
-    # plt.show()
+    # arrayF2dAbs = np.array(F2dAbs)
+    # print(arrayF2dAbs)
+    # x, y = np.meshgrid(u, u)
+    #
+    # fig = pylab.figure()
+    # axes = Axes3D(fig)
+    # axes.plot_surface(x, y, arrayF2dAbs, cmap=plt.cm.jet)
+    # pylab.savefig("plots/ai2d_u(" + str(u_left) + ", " + str(u_right) + ", " + str(Nu) + ")_x(" + str(x_left) + ", " + str(x_right) + ", " + str(Nx) + ")_param(" + str(Aiparam) + ").png")
+    # pylab.show()
+    print("another done")
+    plt.plot(u, Fabs, u, G)
+    plt.grid()
+    plt.legend(['Numeric', 'mp.quad'])
+    plt.savefig("plots/ai_u(" + str(u_left) + ", " + str(u_right) + ", " + str(Nu) + ")_x(" + str(x_left) + ", " + str(x_right) + ", " + str(Nx) + ").png")
+    plt.show()
 
 def InitialAiryPhaze():
     x = np.linspace(-2, 2, 400)
@@ -243,12 +397,12 @@ def anotherintegrate():
 def fresnel():
     a = 3
     b = 1
-    Nx = 801
-    Nu = 401
-    u_left = -4
-    u_right = 4
-    x_left = -4
-    x_right = 4
+    Nx = 3001
+    Nu = 1501
+    u_left = -5
+    u_right = 5
+    x_left = -6
+    x_right = 6
 
     x = np.linspace(x_left, x_right, Nx)
     F = [0] * Nu
@@ -263,15 +417,15 @@ def fresnel():
     start = time.clock()
     Fmid = [0]*(Nx-1)
     for i in range(Nx - 1):
-        Fmid[i] = Gauss((x[i] + x[i + 1]) / 2)* (x[i + 1] - x[i])
+        Fmid[i] = Pe((x[i] + x[i + 1]) / 2)* (x[i + 1] - x[i])
     for n in range(Nu):
         F[n] = 0
         for i in range(Nx - 1):
-            F[n] += Fmid[i] * cmath.exp((I * k /(2*z))* ((x[i] + x[i + 1])/2 - u[n])**2)   # сейчас в формуле в экспоненте стоит -I, чтобы было похоже на результат Фурье
+            F[n] += Fmid[i] * np.exp((1j * k /(2*z))* ((x[i] + x[i + 1])/2 - u[n])**2)   # сейчас в формуле в экспоненте стоит -I, чтобы было похоже на результат Фурье
 
         # F[n] *=
             # print(F[j])
-    F = list(map(lambda x: x*cmath.exp(1j * k * z) * np.sqrt(1j * k / (2 * np.pi * z)), F))
+    F = list(map(lambda x: x*np.exp(1j * k * z) * np.sqrt(1j * k / (2 * np.pi * z)), F))
     print(np.sqrt(-1j * k / (2 * np.pi * z)))
     print('a')
     # F = np.fft.fftshift(F)
@@ -293,7 +447,7 @@ def fresnel():
     # print(Fabs)
     plt.plot(u, Fabs)
     plt.grid()
-    plt.savefig("plots/fresnelGauss_u(" + str(u_left) + ", " + str(u_right) + ", " + str(Nu) + ")_x(" + str(x_left) + ", " + str(x_right) + ", " + str(Nx) + ")_param(" + str(Aiparam) + ")_z(" + str(z) + ").png")
+    plt.savefig("plots/fresnel_pe_u(" + str(u_left) + ", " + str(u_right) + ", " + str(Nu) + ")_x(" + str(x_left) + ", " + str(x_right) + ", " + str(Nx) + ")_param(" + str(pe_param) + ")_z(" + str(z) + ").png")
     plt.show()
 
 def scipyfresnel():
@@ -319,5 +473,8 @@ def scipyfresnel():
 # quadint()
 # a()
 # anotherintegrate()
-fresnel()
+# fresnel()
 # scipyfresnel()
+# getInitPe2d()
+# Fourier2d()
+halfPearcey()
